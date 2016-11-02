b21823ffcdd45775569a9c4ac1aafc33b67278df
Henkelmann, Patrick <patrick.henkelmann@student.fhws.de>
2016-11-02 16:25:37 +0100

    Create Übung_2

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Hello
+{
+    public static Stopwatch stopwatch = new Stopwatch();
+    public static void Main()
+    {
+        Console.WriteLine("Hello World");
+        stopwatch.Reset();
+        long[] times = new long[4];
+        times[0] = HelloWorld(10);
+        times[1] = HelloWorld(100);
+        times[2] = HelloWorld(1000);
+        times[3] = HelloWorld(10000);
+        
+        foreach(long time in times)
+        {
+            Console.WriteLine(time);
+        }
+        Thread.Sleep(100000);
+
+    }
+
+    public static long HelloWorld(int repeat)
+    {
+        stopwatch.Start();
+        for(int i = 0; i < repeat; ++i)
+        {
+            Console.WriteLine("Hello World");
+        }
+        stopwatch.Stop();
+        long time = stopwatch.ElapsedMilliseconds;
+        stopwatch.Reset();
+        return time;
+    }
+}
---
+++ prog3/prog3/uhrzeit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int min;
+	int hour;
+	int sec;
+	cout << "Geben Sie die aktuelle Uhrzeit ein" << endl;
+	cout << "Stunden: ";
+	cin >> hour;
+	cout << "\nMinuten: ";
+	cin >> min;
+	cout << "\nSekunden: ";
+	cin >> sec;
+
+}
+
+int calcSec(int sec, int ident) {
+	if (ident == 0) {
+		return 
+	}
+}
