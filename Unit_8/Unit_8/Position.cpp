3ca1c84a9241434adce72de07c808bb8337baf14
Henkelmann, Patrick <patrick.henkelmann@student.fhws.de>
2017-01-16 16:49:06 +0100

    Position init members from string to int

--- Unit_8/Unit_8/Position.cpp
+++ Unit_8/Unit_8/Position.cpp
@@ -1,5 +1,56 @@
-
+#include <iostream>
+#include <string>
 
 class Position {
 
+public:
+	Position(std::string rank, std::string name, std::string country, std::string age, std::string wealth, std::string sourceOfWealth) :
+		name(name), country(country), sourceOfWealth(sourceOfWealth) {
+
+		setRank(rank);
+		setAge(age);
+		setWealth(wealth);
+	}
+
+	friend std::ostream& operator<<(std::ostream&, const Position&);
+
+private:
+	void setRank(std::string rankVal) {
+		try {
+			rank = std::stoi(rankVal);
+		}
+		catch (const std::invalid_argument &ia) {
+			rank = -1;
+		}
+	}
+	void setAge(std::string ageVal) {
+		try {
+			age = std::stoi(ageVal);
+		}
+		catch (const std::invalid_argument &ia) {
+			age = -1;
+		}
+	}
+	void setWealth(std::string wealthVal) {
+		try {
+			wealth = std::stof(wealthVal);
+		}
+		catch (const std::invalid_argument &ia) {
+			wealthVal = -1;
+		}
+	}
+public:
+	int rank;
+	std::string name;
+	std::string country;
+	int age;
+	float wealth;
+	std::string sourceOfWealth;
 };
+
+/*std::ostream& operator<<(std::ostream& out, const Position &position)
+{
+	out << position.rank << " " << position.name << " " << position.country << " " << position.age << " "
+		<< position.averageWealth << " " << position.sourceOfWealth << "\n";
+	return out;
+}*/
--- Unit_8/Unit_8/WordWealthListLoader.cpp
+++ Unit_8/Unit_8/WordWealthListLoader.cpp
@@ -1,53 +1,92 @@
 #include <fstream>
-#include <iostream>
 #include <string>
-#include "DataLoader.cpp"
+#include <vector>
+#include "Position.cpp"
+#include <sstream>
 
-class WorldWealthListLoader:DataLoader {
+class WorldWealthListLoader {
 
 public:
-	WorldWealthListLoader(std::string filename):filename(filename) {}
-
-	// Geerbt über DataLoader
-	Position getNextPosition() override
-	{
-		return Position();
+	WorldWealthListLoader(std::string filename):filename(filename) {
+		open();
+		ignoreHeader();
 	}
 
-	int hasNextPosition() override
+	// Geerbt über DataLoader
+	std::vector<Position> getPosition()
 	{
-		return 0;
+		std::string line;
+		std::vector<Position> position;		
+		while (std::getline(input, line)) {
+			if (static_cast<int>(line.at(0)) > 47 && static_cast<int>(line.at(0)) < 58) {
+				std::vector<std::string> pos = split(line, '\t');
+				if (pos.size() == 6) {
+
+					position.push_back(*new Position(pos.at(0), pos.at(1), pos.at(2), pos.at(3), pos.at(4), pos.at(5)));
+				}	
+			}
+				
+		}
+		close();
+		return position;
+		
+	}
+
+	void close(){
+		input.close();
 	}
 
 private:
 	std::string filename;
 	std::ifstream input;
 
+	std::vector<std::string> split(const std::string& str, char delimiter)
+	{
+		std::istringstream is(str);
+		std::vector<std::string> result;
+		for (std::string cur; std::getline(is, cur, delimiter); 
+		result.push_back(cur));
+		return result;
+	}
+
 	int open() {
-		input = std::ifstream(filename);
+		input = std::ifstream(filename, std::ifstream::in);
 		if (!input) {
-			std::cerr << "Fehler beim Oeffnen der Date " << filename << std::endl;
+			std::cerr << "Fehler beim Oeffnen der Datei " << filename << std::endl;
 			return 0;
 		}
 		
 		return 1;
 	}
+	void ignoreHeader() {
+		std::string line;
+		std::getline(input, line);
+		std::getline(input, line);
+	}
 };
 
 int main() {
-	std::ifstream input("WorldWealthList.txt");
+	WorldWealthListLoader* wwl = new WorldWealthListLoader("WorldWealthList.txt");
+	WorldWealthListLoader& wwlRef = *wwl;
+	std::cout << "hallo 1" << std::endl;
 
-	if (!input)
-	{
-		std::cerr << "Fehler beim Oeffnen der Datei " << "  sdf"<< "\n";
-		return 1;
+	std::vector<Position> positions;
+	Position* currentPosition;
+
+	/*while ((currentPosition = wwlRef.getNextPosition()) != nullptr) {
+		positions.push_back(*currentPosition);
 	}
+	wwlRef.close();*/
+	positions = wwlRef.getPosition();
 
-
-
-	std::string line;
-	while (std::getline(input, line)) {
-		std::cout << line << "\n";
+	for (Position position : positions) {
+		std::cout << position.rank << " " << position.name << " " << position.country << " " << position.age << " "
+			<< position.wealth << " " << position.sourceOfWealth << "\n";
+		
 	}
+	std::cout << "hallo" << std::endl;
 	return 0;
+	
+
+
 }
